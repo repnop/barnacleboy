cfca5a4ace0d412dbd4e43054323471d81831361
rep-nop <repnop@outlook.com>
2017-01-27 16:22:21 -0500

    Updated gitignore

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -1,5 +1,9 @@
-pub mod cpu {
-    pub struct Cpu {
-        
-    }
+pub struct Cpu {
+    registers: [u8; 8],
+    stack_pointer: u16;
+    program_counter: u16;
+}
+
+impl Cpu {
+    pub fn new 
 }
