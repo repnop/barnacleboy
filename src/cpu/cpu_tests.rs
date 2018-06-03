eff995500ada471e427e102de0e7b8e06455fef1
rep-nop <repnop@outlook.com>
2018-06-02 20:09:18 -0400

    Splits CPU tests into their own file.

---
+++ src/cpu/cpu_tests.rs
@@ -0,0 +1,335 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Debug, Default)]
+    struct DummyMemInterface {
+        mem: [u8; 32],
+    }
+
+    impl MemoryInterface for DummyMemInterface {
+        type Word = u8;
+        type Index = u16;
+        type Error = LRError;
+
+        fn read(&self, address: Self::Index) -> Result<Self::Word, Self::Error> {
+            Ok(self.mem[address as usize])
+        }
+        fn write(&mut self, address: Self::Index, data: Self::Word) -> Result<(), Self::Error> {
+            self.mem[address as usize] = data;
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn flags() {
+        let mut cpu = SharpLR35902::new(Rc::new(RefCell::new(DummyMemInterface::default())));
+
+        cpu.registers.set_c();
+        assert!(cpu.registers.c());
+
+        cpu.registers.set_s();
+        assert!(cpu.registers.s());
+
+        cpu.registers.set_z();
+        assert!(cpu.registers.z());
+
+        cpu.registers.set_h();
+        assert!(cpu.registers.h());
+
+        cpu.registers.clear_c();
+        assert!(!cpu.registers.c());
+
+        cpu.registers.clear_s();
+        assert!(!cpu.registers.s());
+
+        cpu.registers.clear_z();
+        assert!(!cpu.registers.z());
+
+        cpu.registers.clear_h();
+        assert!(!cpu.registers.h());
+    }
+
+    #[test]
+    fn registers() {
+        let mut cpu = SharpLR35902::new(Rc::new(RefCell::new(DummyMemInterface::default())));
+
+        cpu.registers.a = 0x11;
+        cpu.registers.f = 0x22;
+        cpu.registers.b = 0x33;
+        cpu.registers.c = 0x44;
+        cpu.registers.d = 0x55;
+        cpu.registers.e = 0x66;
+        cpu.registers.h = 0x77;
+        cpu.registers.l = 0x88;
+
+        assert_eq!(cpu.registers.as_dwords().af, 0x1122);
+        assert_eq!(cpu.registers.as_dwords().bc, 0x3344);
+        assert_eq!(cpu.registers.as_dwords().de, 0x5566);
+        assert_eq!(cpu.registers.as_dwords().hl, 0x7788);
+    }
+
+    #[test]
+    fn opcode_bits() {
+        let opcode = 0b1010_1010;
+        let bits = OpcodeBits::from(opcode);
+
+        assert_eq!(bits.x, 0b10);
+        assert_eq!(bits.y, 0b101);
+        assert_eq!(bits.z, 0b010);
+        assert_eq!(bits.p, 0b10);
+        assert_eq!(bits.q, 0b1);
+    }
+
+    #[test]
+    fn ld_at_bc_a() {
+        let mut cpu = SharpLR35902::new(Rc::new(RefCell::new(DummyMemInterface::default())));
+        cpu.registers.a = 0xFF;
+        cpu.registers.as_dwords().bc = 0x01;
+        assert!(super::ld_at_bc_a(&mut cpu, 0x02).is_ok());
+        assert_eq!(cpu.read(0x01).unwrap(), 0xFF);
+    }
+
+    #[test]
+    fn ld_at_de_a() {
+        let mut cpu = SharpLR35902::new(Rc::new(RefCell::new(DummyMemInterface::default())));
+        cpu.registers.a = 0xFF;
+        cpu.registers.as_dwords().de = 0x01;
+        assert!(super::ld_at_de_a(&mut cpu, 0x12).is_ok());
+        assert_eq!(cpu.read(0x01).unwrap(), 0xFF);
+    }
+
+    #[test]
+    fn ld_at_hli_a() {
+        let mut cpu = SharpLR35902::new(Rc::new(RefCell::new(DummyMemInterface::default())));
+        cpu.registers.a = 0xFF;
+        cpu.registers.as_dwords().hl = 0x01;
+        assert!(super::ld_at_hli_a(&mut cpu, 0x22).is_ok());
+        assert_eq!(cpu.read(0x01).unwrap(), 0xFF);
+        assert_eq!(cpu.registers.as_dwords().hl, 0x02);
+    }
+
+    #[test]
+    fn ld_at_hld_a() {
+        let mut cpu = SharpLR35902::new(Rc::new(RefCell::new(DummyMemInterface::default())));
+        cpu.registers.a = 0xFF;
+        cpu.registers.as_dwords().hl = 0x01;
+        assert!(super::ld_at_hld_a(&mut cpu, 0x22).is_ok());
+        assert_eq!(cpu.read(0x01).unwrap(), 0xFF);
+        assert_eq!(cpu.registers.as_dwords().hl, 0x00);
+    }
+
+    #[test]
+    fn ld_r_d8() {
+        let mut cpu = SharpLR35902::new(Rc::new(RefCell::new(DummyMemInterface::default())));
+
+        for i in 0u8..8u8 {
+            cpu.write(i as u16, i + 1).unwrap();
+        }
+
+        assert!(super::ld_r_d8(&mut cpu, 0x06).is_ok());
+        assert_eq!(cpu.registers.b, 0x01);
+
+        assert!(super::ld_r_d8(&mut cpu, 0x0E).is_ok());
+        assert_eq!(cpu.registers.c, 0x02);
+
+        assert!(super::ld_r_d8(&mut cpu, 0x16).is_ok());
+        assert_eq!(cpu.registers.d, 0x03);
+
+        assert!(super::ld_r_d8(&mut cpu, 0x1E).is_ok());
+        assert_eq!(cpu.registers.e, 0x04);
+
+        assert!(super::ld_r_d8(&mut cpu, 0x26).is_ok());
+        assert_eq!(cpu.registers.h, 0x05);
+
+        assert!(super::ld_r_d8(&mut cpu, 0x2E).is_ok());
+        assert_eq!(cpu.registers.l, 0x06);
+
+        cpu.registers.as_dwords().hl = 0x0000;
+        assert!(super::ld_r_d8(&mut cpu, 0x36).is_ok());
+        assert_eq!(cpu.read_hl().unwrap(), 0x07);
+
+        assert!(super::ld_r_d8(&mut cpu, 0x3E).is_ok());
+        assert_eq!(cpu.registers.a, 0x08);
+    }
+
+    #[test]
+    fn ld_r_at_hl() {
+        let mut cpu = SharpLR35902::new(Rc::new(RefCell::new(DummyMemInterface::default())));
+
+        cpu.write(0x0000, 0xFF).unwrap();
+
+        assert!(super::ld_r_at_hl(&mut cpu, 0x46).is_ok());
+        assert_eq!(cpu.registers.b, 0xFF);
+
+        assert!(super::ld_r_at_hl(&mut cpu, 0x4E).is_ok());
+        assert_eq!(cpu.registers.c, 0xFF);
+
+        assert!(super::ld_r_at_hl(&mut cpu, 0x56).is_ok());
+        assert_eq!(cpu.registers.d, 0xFF);
+
+        assert!(super::ld_r_at_hl(&mut cpu, 0x5E).is_ok());
+        assert_eq!(cpu.registers.e, 0xFF);
+
+        assert!(super::ld_r_at_hl(&mut cpu, 0x66).is_ok());
+        assert_eq!(cpu.registers.h, 0xFF);
+
+        cpu.registers.h = 0x00;
+        assert!(super::ld_r_at_hl(&mut cpu, 0x6E).is_ok());
+        assert_eq!(cpu.registers.l, 0xFF);
+
+        // 0x76 is the instruction `HALT`
+        cpu.registers.l = 0x00;
+        assert!(super::ld_r_at_hl(&mut cpu, 0x7E).is_ok());
+        assert_eq!(cpu.registers.a, 0xFF);
+    }
+
+    #[test]
+    fn ld_at_hl_r() {
+        let mut cpu = SharpLR35902::new(Rc::new(RefCell::new(DummyMemInterface::default())));
+
+        cpu.registers.as_dwords().hl = 0x0011;
+        cpu.registers.b = 0x01;
+        cpu.registers.c = 0x02;
+        cpu.registers.d = 0x03;
+        cpu.registers.e = 0x04;
+        cpu.registers.a = 0x05;
+
+        assert!(super::ld_at_hl_r(&mut cpu, 0x70).is_ok());
+        assert_eq!(cpu.read_hl().unwrap(), 0x01);
+
+        assert!(super::ld_at_hl_r(&mut cpu, 0x71).is_ok());
+        assert_eq!(cpu.read_hl().unwrap(), 0x02);
+
+        assert!(super::ld_at_hl_r(&mut cpu, 0x72).is_ok());
+        assert_eq!(cpu.read_hl().unwrap(), 0x03);
+
+        assert!(super::ld_at_hl_r(&mut cpu, 0x73).is_ok());
+        assert_eq!(cpu.read_hl().unwrap(), 0x04);
+
+        assert!(super::ld_at_hl_r(&mut cpu, 0x74).is_ok());
+        assert_eq!(cpu.read_hl().unwrap(), 0x00);
+
+        assert!(super::ld_at_hl_r(&mut cpu, 0x75).is_ok());
+        assert_eq!(cpu.read_hl().unwrap(), 0x11);
+
+        // 0x76 is the instruction `HALT`
+        assert!(super::ld_at_hl_r(&mut cpu, 0x77).is_ok());
+        assert_eq!(cpu.read_hl().unwrap(), 0x05);
+    }
+
+    #[test]
+    fn ld_at_hl_d8() {
+        let mut cpu = SharpLR35902::new(Rc::new(RefCell::new(DummyMemInterface::default())));
+
+        cpu.registers.as_dwords().hl = 0x0011;
+        cpu.write(0x00, 0xFF).unwrap();
+
+        assert!(super::ld_at_hl_d8(&mut cpu, 0x36).is_ok());
+        assert_eq!(cpu.read_hl().unwrap(), 0xFF);
+    }
+
+    #[test]
+    fn ld_a_at_bc() {
+        let mut cpu = SharpLR35902::new(Rc::new(RefCell::new(DummyMemInterface::default())));
+
+        cpu.write(0x0A, 0xFF).unwrap();
+        cpu.registers.as_dwords().bc = 0x0A;
+
+        assert!(super::ld_a_at_bc(&mut cpu, 0x0A).is_ok());
+        assert_eq!(cpu.registers.a, 0xFF);
+    }
+
+    #[test]
+    fn helper_tests() {
+        let result = AluResult {
+            result: 0xFFu8,
+            zero: false,
+            carry: false,
+            half_carry: false,
+            subtract: false,
+        };
+        assert_eq!(u8_add(0xF0, 0x0F), result);
+
+        let result = AluResult {
+            result: 0x10u8,
+            zero: false,
+            carry: false,
+            half_carry: true,
+            subtract: false,
+        };
+        assert_eq!(u8_add(0x0F, 0x01), result);
+
+        let result = AluResult {
+            result: 0x00u8,
+            zero: true,
+            carry: true,
+            half_carry: true,
+            subtract: false,
+        };
+        assert_eq!(u8_add(0xFF, 0x01), result);
+
+        let result = AluResult {
+            result: 0xEFu8,
+            zero: false,
+            carry: false,
+            half_carry: true,
+            subtract: true,
+        };
+        assert_eq!(u8_sub(0xF0, 0x01), result);
+
+        let result = AluResult {
+            result: 0xFFu8,
+            zero: false,
+            carry: true,
+            half_carry: true,
+            subtract: true,
+        };
+        assert_eq!(u8_sub(0x00, 0x01), result);
+
+        let result = AluResult {
+            result: 0xFFFFu16,
+            zero: false,
+            carry: false,
+            half_carry: false,
+            subtract: false,
+        };
+        assert_eq!(u16_add(0xFF00, 0x00FF), result);
+
+        let result = AluResult {
+            result: 0x1000u16,
+            zero: false,
+            carry: false,
+            half_carry: true,
+            subtract: false,
+        };
+        assert_eq!(u16_add(0x0FFF, 0x0001), result);
+
+        let result = AluResult {
+            result: 0x00u16,
+            zero: true,
+            carry: true,
+            half_carry: true,
+            subtract: false,
+        };
+        assert_eq!(u16_add(0xFFFF, 0x0001), result);
+
+        let result = AluResult {
+            result: 0xEF00u16,
+            zero: false,
+            carry: false,
+            half_carry: true,
+            subtract: true,
+        };
+        assert_eq!(u16_sub(0xFEFF, 0x0FFF), result);
+
+        let result = AluResult {
+            result: 0xFFFFu16,
+            zero: false,
+            carry: true,
+            half_carry: true,
+            subtract: true,
+        };
+        assert_eq!(u16_sub(0x0000, 0x0001), result);
+    }
+}
--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,5 +1,9 @@
+#[cfg(test)]
+mod cpu_tests;
+
 use memory::MemoryInterface;
 use std::{cell::RefCell, rc::Rc};
+
 /// Zero flag.
 const F_ZERO: u8 = 0b1000_0000;
 /// Subtraction flag.
@@ -297,7 +301,7 @@ impl SharpLR35902 {
         }
     }
 
-    /// Reads a byte at the program counter and increments.
+    /// Reads a word at the program counter and increments.
     fn read_instruction_word(&mut self) -> Result<u8, LRError> {
         let pc = self.registers.pc;
         self.registers.pc += 1;
@@ -305,7 +309,7 @@ impl SharpLR35902 {
         Ok(self.memory_controller.borrow().read(pc)?)
     }
 
-    /// Reads a word at the program counter and increments.
+    /// Reads a dword at the program counter and increments twice.
     fn read_instruction_dword(&mut self) -> Result<u16, LRError> {
         let first_byte = self.read_instruction_word()?;
         let second_byte = self.read_instruction_word()?;
@@ -344,7 +348,7 @@ impl SharpLR35902 {
 /// Groups of bits in an opcode.
 ///
 /// Broken up into the following groups:
-///
+///```
 /// Bits 6-7: x
 ///
 /// Bits 3-5: y
@@ -354,6 +358,7 @@ impl SharpLR35902 {
 ///     Bit 3: q
 ///
 /// Bits 0-2: z
+/// ```
 struct OpcodeBits {
     x: u8,
     y: u8,
@@ -510,7 +515,7 @@ fn ld_at_hl_r(cpu: &mut SharpLR35902, opcode: u8) -> SharpResult {
 /// pointed to by register `HL`.
 ///
 /// Flags affected: none
-fn ld_at_hl_d8(cpu: &mut SharpLR35902, opcode: u8) -> SharpResult {
+fn ld_at_hl_d8(cpu: &mut SharpLR35902, _: u8) -> SharpResult {
     let val = cpu.read_instruction_word()?;
     cpu.write_hl(val)?;
 
@@ -521,7 +526,7 @@ fn ld_at_hl_d8(cpu: &mut SharpLR35902, opcode: u8) -> SharpResult {
 /// register.
 ///
 /// Flags affected: none
-fn ld_a_at_bc(cpu: &mut SharpLR35902, opcode: u8) -> SharpResult {
+fn ld_a_at_bc(cpu: &mut SharpLR35902, _: u8) -> SharpResult {
     let addr = cpu.registers.as_dwords().bc;
     cpu.registers.a = cpu.read(addr)?;
 
@@ -532,7 +537,7 @@ fn ld_a_at_bc(cpu: &mut SharpLR35902, opcode: u8) -> SharpResult {
 /// register.
 ///
 /// Flags affected: none
-fn ld_a_at_de(cpu: &mut SharpLR35902, opcode: u8) -> SharpResult {
+fn ld_a_at_de(cpu: &mut SharpLR35902, _: u8) -> SharpResult {
     let addr = cpu.registers.as_dwords().de;
     cpu.registers.a = cpu.read(addr)?;
 
@@ -543,7 +548,7 @@ fn ld_a_at_de(cpu: &mut SharpLR35902, opcode: u8) -> SharpResult {
 /// register.
 ///
 /// Flags affected: none
-fn ld_a_at_c(cpu: &mut SharpLR35902, opcode: u8) -> SharpResult {
+fn ld_a_at_c(cpu: &mut SharpLR35902, _: u8) -> SharpResult {
     let addr = cpu.registers.c as u16 + 0xFF00;
     cpu.registers.a = cpu.read(addr)?;
 
@@ -554,7 +559,7 @@ fn ld_a_at_c(cpu: &mut SharpLR35902, opcode: u8) -> SharpResult {
 /// to by register `C` + 0xFF00.
 ///
 /// Flags affected: none
-fn ld_at_c_a(cpu: &mut SharpLR35902, opcode: u8) -> SharpResult {
+fn ld_at_c_a(cpu: &mut SharpLR35902, _: u8) -> SharpResult {
     let addr = cpu.registers.c as u16 + 0xFF00;
     let a = cpu.registers.a;
     cpu.write(addr, a)?;
@@ -566,7 +571,7 @@ fn ld_at_c_a(cpu: &mut SharpLR35902, opcode: u8) -> SharpResult {
 /// into the `A` register.
 ///
 /// Flags affected: none
-fn ld_a_at_d8(cpu: &mut SharpLR35902, opcode: u8) -> SharpResult {
+fn ld_a_at_d8(cpu: &mut SharpLR35902, _: u8) -> SharpResult {
     let addr = cpu.read_instruction_word()? as u16 + 0xFF00;
     cpu.registers.a = cpu.read(addr)?;
 
@@ -577,7 +582,7 @@ fn ld_a_at_d8(cpu: &mut SharpLR35902, opcode: u8) -> SharpResult {
 /// to by the immediate 8-bit operand `d8` + 0xFF00.
 ///
 /// Flags affected: none
-fn ld_at_d8_a(cpu: &mut SharpLR35902, opcode: u8) -> SharpResult {
+fn ld_at_d8_a(cpu: &mut SharpLR35902, _: u8) -> SharpResult {
     let addr = cpu.read_instruction_word()? as u16 + 0xFF00;
     let a = cpu.registers.a;
     cpu.write(addr, a)?;
@@ -589,7 +594,7 @@ fn ld_at_d8_a(cpu: &mut SharpLR35902, opcode: u8) -> SharpResult {
 /// into the `A` register.
 ///
 /// Flags affected: none
-fn ld_a_at_d16(cpu: &mut SharpLR35902, opcode: u8) -> SharpResult {
+fn ld_a_at_d16(cpu: &mut SharpLR35902, _: u8) -> SharpResult {
     let addr = cpu.read_instruction_dword()?;
     cpu.registers.a = cpu.read(addr)?;
 
@@ -600,7 +605,7 @@ fn ld_a_at_d16(cpu: &mut SharpLR35902, opcode: u8) -> SharpResult {
 /// to by the immediate 16-bit operand `d16`
 ///
 /// Flags affected: none
-fn ld_at_d16_a(cpu: &mut SharpLR35902, opcode: u8) -> SharpResult {
+fn ld_at_d16_a(cpu: &mut SharpLR35902, _: u8) -> SharpResult {
     let addr = cpu.read_instruction_dword()?;
     let a = cpu.registers.a;
     cpu.write(addr, a)?;
@@ -1032,329 +1037,4 @@ fn i16_add(first: i16, second: i16) -> AluResult<i16> {
     chc.subtract = false;
 
     chc
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[derive(Debug, Default)]
-    struct DummyMemInterface {
-        mem: [u8; 32],
-    }
-
-    impl MemoryInterface for DummyMemInterface {
-        type Word = u8;
-        type Index = u16;
-        type Error = LRError;
-
-        fn read(&self, address: Self::Index) -> Result<Self::Word, Self::Error> {
-            Ok(self.mem[address as usize])
-        }
-        fn write(&mut self, address: Self::Index, data: Self::Word) -> Result<(), Self::Error> {
-            self.mem[address as usize] = data;
-            Ok(())
-        }
-    }
-
-    #[test]
-    fn flags() {
-        let mut cpu = SharpLR35902::new(Rc::new(RefCell::new(DummyMemInterface::default())));
-
-        cpu.registers.set_c();
-        assert!(cpu.registers.c());
-
-        cpu.registers.set_s();
-        assert!(cpu.registers.s());
-
-        cpu.registers.set_z();
-        assert!(cpu.registers.z());
-
-        cpu.registers.set_h();
-        assert!(cpu.registers.h());
-
-        cpu.registers.clear_c();
-        assert!(!cpu.registers.c());
-
-        cpu.registers.clear_s();
-        assert!(!cpu.registers.s());
-
-        cpu.registers.clear_z();
-        assert!(!cpu.registers.z());
-
-        cpu.registers.clear_h();
-        assert!(!cpu.registers.h());
-    }
-
-    #[test]
-    fn registers() {
-        let mut cpu = SharpLR35902::new(Rc::new(RefCell::new(DummyMemInterface::default())));
-
-        cpu.registers.a = 0x11;
-        cpu.registers.f = 0x22;
-        cpu.registers.b = 0x33;
-        cpu.registers.c = 0x44;
-        cpu.registers.d = 0x55;
-        cpu.registers.e = 0x66;
-        cpu.registers.h = 0x77;
-        cpu.registers.l = 0x88;
-
-        assert_eq!(cpu.registers.as_dwords().af, 0x1122);
-        assert_eq!(cpu.registers.as_dwords().bc, 0x3344);
-        assert_eq!(cpu.registers.as_dwords().de, 0x5566);
-        assert_eq!(cpu.registers.as_dwords().hl, 0x7788);
-    }
-
-    #[test]
-    fn opcode_bits() {
-        let opcode = 0b1010_1010;
-        let bits = OpcodeBits::from(opcode);
-
-        assert_eq!(bits.x, 0b10);
-        assert_eq!(bits.y, 0b101);
-        assert_eq!(bits.z, 0b010);
-        assert_eq!(bits.p, 0b10);
-        assert_eq!(bits.q, 0b1);
-    }
-
-    #[test]
-    fn ld_at_bc_a() {
-        let mut cpu = SharpLR35902::new(Rc::new(RefCell::new(DummyMemInterface::default())));
-        cpu.registers.a = 0xFF;
-        cpu.registers.as_dwords().bc = 0x01;
-        assert!(super::ld_at_bc_a(&mut cpu, 0x02).is_ok());
-        assert_eq!(cpu.read(0x01).unwrap(), 0xFF);
-    }
-
-    #[test]
-    fn ld_at_de_a() {
-        let mut cpu = SharpLR35902::new(Rc::new(RefCell::new(DummyMemInterface::default())));
-        cpu.registers.a = 0xFF;
-        cpu.registers.as_dwords().de = 0x01;
-        assert!(super::ld_at_de_a(&mut cpu, 0x12).is_ok());
-        assert_eq!(cpu.read(0x01).unwrap(), 0xFF);
-    }
-
-    #[test]
-    fn ld_at_hli_a() {
-        let mut cpu = SharpLR35902::new(Rc::new(RefCell::new(DummyMemInterface::default())));
-        cpu.registers.a = 0xFF;
-        cpu.registers.as_dwords().hl = 0x01;
-        assert!(super::ld_at_hli_a(&mut cpu, 0x22).is_ok());
-        assert_eq!(cpu.read(0x01).unwrap(), 0xFF);
-        assert_eq!(cpu.registers.as_dwords().hl, 0x02);
-    }
-
-    #[test]
-    fn ld_at_hld_a() {
-        let mut cpu = SharpLR35902::new(Rc::new(RefCell::new(DummyMemInterface::default())));
-        cpu.registers.a = 0xFF;
-        cpu.registers.as_dwords().hl = 0x01;
-        assert!(super::ld_at_hld_a(&mut cpu, 0x22).is_ok());
-        assert_eq!(cpu.read(0x01).unwrap(), 0xFF);
-        assert_eq!(cpu.registers.as_dwords().hl, 0x00);
-    }
-
-    #[test]
-    fn ld_r_d8() {
-        let mut cpu = SharpLR35902::new(Rc::new(RefCell::new(DummyMemInterface::default())));
-
-        for i in 0u8..8u8 {
-            cpu.write(i as u16, i + 1).unwrap();
-        }
-
-        assert!(super::ld_r_d8(&mut cpu, 0x06).is_ok());
-        assert_eq!(cpu.registers.b, 0x01);
-
-        assert!(super::ld_r_d8(&mut cpu, 0x0E).is_ok());
-        assert_eq!(cpu.registers.c, 0x02);
-
-        assert!(super::ld_r_d8(&mut cpu, 0x16).is_ok());
-        assert_eq!(cpu.registers.d, 0x03);
-
-        assert!(super::ld_r_d8(&mut cpu, 0x1E).is_ok());
-        assert_eq!(cpu.registers.e, 0x04);
-
-        assert!(super::ld_r_d8(&mut cpu, 0x26).is_ok());
-        assert_eq!(cpu.registers.h, 0x05);
-
-        assert!(super::ld_r_d8(&mut cpu, 0x2E).is_ok());
-        assert_eq!(cpu.registers.l, 0x06);
-
-        cpu.registers.as_dwords().hl = 0x0000;
-        assert!(super::ld_r_d8(&mut cpu, 0x36).is_ok());
-        assert_eq!(cpu.read_hl().unwrap(), 0x07);
-
-        assert!(super::ld_r_d8(&mut cpu, 0x3E).is_ok());
-        assert_eq!(cpu.registers.a, 0x08);
-    }
-
-    #[test]
-    fn ld_r_at_hl() {
-        let mut cpu = SharpLR35902::new(Rc::new(RefCell::new(DummyMemInterface::default())));
-
-        cpu.write(0x0000, 0xFF).unwrap();
-
-        assert!(super::ld_r_at_hl(&mut cpu, 0x46).is_ok());
-        assert_eq!(cpu.registers.b, 0xFF);
-
-        assert!(super::ld_r_at_hl(&mut cpu, 0x4E).is_ok());
-        assert_eq!(cpu.registers.c, 0xFF);
-
-        assert!(super::ld_r_at_hl(&mut cpu, 0x56).is_ok());
-        assert_eq!(cpu.registers.d, 0xFF);
-
-        assert!(super::ld_r_at_hl(&mut cpu, 0x5E).is_ok());
-        assert_eq!(cpu.registers.e, 0xFF);
-
-        assert!(super::ld_r_at_hl(&mut cpu, 0x66).is_ok());
-        assert_eq!(cpu.registers.h, 0xFF);
-
-        cpu.registers.h = 0x00;
-        assert!(super::ld_r_at_hl(&mut cpu, 0x6E).is_ok());
-        assert_eq!(cpu.registers.l, 0xFF);
-
-        // 0x76 is the instruction `HALT`
-        cpu.registers.l = 0x00;
-        assert!(super::ld_r_at_hl(&mut cpu, 0x7E).is_ok());
-        assert_eq!(cpu.registers.a, 0xFF);
-    }
-
-    #[test]
-    fn ld_at_hl_r() {
-        let mut cpu = SharpLR35902::new(Rc::new(RefCell::new(DummyMemInterface::default())));
-
-        cpu.registers.as_dwords().hl = 0x0011;
-        cpu.registers.b = 0x01;
-        cpu.registers.c = 0x02;
-        cpu.registers.d = 0x03;
-        cpu.registers.e = 0x04;
-        cpu.registers.a = 0x05;
-
-        assert!(super::ld_at_hl_r(&mut cpu, 0x70).is_ok());
-        assert_eq!(cpu.read_hl().unwrap(), 0x01);
-
-        assert!(super::ld_at_hl_r(&mut cpu, 0x71).is_ok());
-        assert_eq!(cpu.read_hl().unwrap(), 0x02);
-
-        assert!(super::ld_at_hl_r(&mut cpu, 0x72).is_ok());
-        assert_eq!(cpu.read_hl().unwrap(), 0x03);
-
-        assert!(super::ld_at_hl_r(&mut cpu, 0x73).is_ok());
-        assert_eq!(cpu.read_hl().unwrap(), 0x04);
-
-        assert!(super::ld_at_hl_r(&mut cpu, 0x74).is_ok());
-        assert_eq!(cpu.read_hl().unwrap(), 0x00);
-
-        assert!(super::ld_at_hl_r(&mut cpu, 0x75).is_ok());
-        assert_eq!(cpu.read_hl().unwrap(), 0x11);
-
-        // 0x76 is the instruction `HALT`
-        assert!(super::ld_at_hl_r(&mut cpu, 0x77).is_ok());
-        assert_eq!(cpu.read_hl().unwrap(), 0x05);
-    }
-
-    #[test]
-    fn ld_at_hl_d8() {
-        let mut cpu = SharpLR35902::new(Rc::new(RefCell::new(DummyMemInterface::default())));
-
-        cpu.registers.as_dwords().hl = 0x0011;
-        cpu.write(0x00, 0xFF).unwrap();
-
-        assert!(super::ld_at_hl_d8(&mut cpu, 0x36).is_ok());
-        assert_eq!(cpu.read_hl().unwrap(), 0xFF);
-    }
-
-    #[test]
-    fn helper_tests() {
-        let result = AluResult {
-            result: 0xFFu8,
-            zero: false,
-            carry: false,
-            half_carry: false,
-            subtract: false,
-        };
-        assert_eq!(u8_add(0xF0, 0x0F), result);
-
-        let result = AluResult {
-            result: 0x10u8,
-            zero: false,
-            carry: false,
-            half_carry: true,
-            subtract: false,
-        };
-        assert_eq!(u8_add(0x0F, 0x01), result);
-
-        let result = AluResult {
-            result: 0x00u8,
-            zero: true,
-            carry: true,
-            half_carry: true,
-            subtract: false,
-        };
-        assert_eq!(u8_add(0xFF, 0x01), result);
-
-        let result = AluResult {
-            result: 0xEFu8,
-            zero: false,
-            carry: false,
-            half_carry: true,
-            subtract: true,
-        };
-        assert_eq!(u8_sub(0xF0, 0x01), result);
-
-        let result = AluResult {
-            result: 0xFFu8,
-            zero: false,
-            carry: true,
-            half_carry: true,
-            subtract: true,
-        };
-        assert_eq!(u8_sub(0x00, 0x01), result);
-
-        let result = AluResult {
-            result: 0xFFFFu16,
-            zero: false,
-            carry: false,
-            half_carry: false,
-            subtract: false,
-        };
-        assert_eq!(u16_add(0xFF00, 0x00FF), result);
-
-        let result = AluResult {
-            result: 0x1000u16,
-            zero: false,
-            carry: false,
-            half_carry: true,
-            subtract: false,
-        };
-        assert_eq!(u16_add(0x0FFF, 0x0001), result);
-
-        let result = AluResult {
-            result: 0x00u16,
-            zero: true,
-            carry: true,
-            half_carry: true,
-            subtract: false,
-        };
-        assert_eq!(u16_add(0xFFFF, 0x0001), result);
-
-        let result = AluResult {
-            result: 0xEF00u16,
-            zero: false,
-            carry: false,
-            half_carry: true,
-            subtract: true,
-        };
-        assert_eq!(u16_sub(0xFEFF, 0x0FFF), result);
-
-        let result = AluResult {
-            result: 0xFFFFu16,
-            zero: false,
-            carry: true,
-            half_carry: true,
-            subtract: true,
-        };
-        assert_eq!(u16_sub(0x0000, 0x0001), result);
-    }
 }
